0513b41504d9ca67078e5498f5f4dd14f12a8eaf
Ryan Kubik <rmkubik@me.com>
2023-04-15 22:18:33 -0700

    Log version and update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom";
 import React from "react";
-
 import App from "./components/App";
+import packageInfo from "../package.json";
+
+console.log(packageInfo.version);
 
 ReactDOM.render(<App />, document.getElementById("root"));
